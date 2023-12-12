a512b0c739857e4ca598f7c96bac9af8f5ff704a
Caio Braga <caioaugustobrg@gmail.com>
2023-12-12 17:47:50 -0300

    :sparkles: feat: user entity

---
+++ src/entities/user.ts
@@ -0,0 +1,61 @@
+import { Entity } from '../core/domain/Entity'
+export interface UserProps {
+  googleId?: string
+  cpf: string
+  name: string
+  email: string
+  phone: string
+  password: string
+  birthDate: Date
+  userRole?: any
+  picture?: string | null
+  speciality?: string | null
+  education?: string | null
+  systemRole: 'CREATED'
+  userRegisterDocument?: string | null
+  userToken?: string | null
+  cep?: string | null
+  professionalExperience?: string | null
+  professionalGoal?: string | null
+}
+
+export class User extends Entity<UserProps> {
+  public googleId?: string
+  public cpf: string
+  public name: string
+  public email: string
+  public phone: string
+  public password: string
+  public birthDate: Date
+  public userRole?: any
+  public picture?: string | null
+  public speciality?: string | null
+  public education?: string | null
+  public systemRole: 'CREATED'
+  public userRegisterDocument?: string | null
+  public userToken?: string | null
+  public cep?: string | null
+  public professionalExperience?: string | null
+  public professionalGoal?: string | null
+
+  public constructor (props: UserProps) {
+    super()
+    this.googleId = props.googleId
+    this.cpf = props.cpf
+    this.name = props.name
+    this.email = props.email
+    this.phone = props.phone
+    this.password = props.password
+    this.birthDate = props.birthDate
+    this.userRole = props.userRole
+    this.picture = props.picture
+    this.speciality = props.speciality
+    this.education = props.education
+    this.systemRole = props.systemRole
+    this.userRegisterDocument = props.userRegisterDocument
+    this.userToken = props.userToken
+    this.cep = props.cep
+    this.professionalExperience = props.professionalExperience
+    this.professionalGoal = props.professionalGoal
+  }
+}
