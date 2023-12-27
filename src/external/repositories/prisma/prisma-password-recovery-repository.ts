052a069ec5cb6c7edfefbe7730c9f7d0cbd61fb3
Caio Braga <caioaugustobrg@gmail.com>
2023-12-26 22:00:06 -0300

    :sparkles: feat: prisma password recovery repository

---
+++ src/external/repositories/prisma/prisma-password-recovery-repository.ts
@@ -0,0 +1,45 @@
+import { type PasswordRecovery } from '../../../entities/password-recovery'
+import { type User } from '../../../entities/user'
+import { type PasswordRecoveryRepository } from '../../../usecases/ports/password-recovery-repository'
+import { PrismaHelper } from './helpers/prisma-helper'
+
+export class PrismaPasswordRecoveryRepository implements PasswordRecoveryRepository {
+  async create (passwordRecovery: PasswordRecovery): Promise<PasswordRecovery> {
+    return (PrismaHelper?.passwordRecovery.create({
+      data: {
+        token: passwordRecovery.token,
+        userId: passwordRecovery.userId,
+        expiration: passwordRecovery.expiration
+      }
+    })) as PasswordRecovery
+  }
+
+  async findUserByEmail (email: string): Promise<User | null> {
+    return (PrismaHelper?.user.findUnique({
+      where: { email }
+    })) as User | null
+  }
+
+  async findToken (userToken: string): Promise<PasswordRecovery | null> {
+    return (PrismaHelper?.passwordRecovery.findFirst({
+      where: { userToken },
+      orderBy: { expiration: 'desc' }
+    })) as PasswordRecovery | null
+  }
+
+  async updatePassword (
+    passwordTrim: string,
+    userId: string
+  ): Promise<User | null> {
+    return (await PrismaHelper.user.update({
+      where: { id: userId },
+      data: { password: passwordTrim }
+    })) as User | null
+  }
+
+  async findUserById (userId: string): Promise<User | null> {
+    return (await PrismaHelper.user.findUnique({
+      where: { id: userId }
+    })) as User | null
+  }
+}
