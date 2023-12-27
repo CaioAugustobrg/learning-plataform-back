0f600206d06c486bca4e97d1078e9c06168c9b2f
Caio Braga <caioaugustobrg@gmail.com>
2023-12-26 22:40:01 -0300

    :sparkles: feat: type for login

--- src/usecases/ports/user-repository.ts
+++ src/usecases/ports/user-repository.ts
@@ -4,6 +4,7 @@ export interface UserRepository {
   findUserByCpf: (cpf: string) => Promise<User | null>
   create: (user: User, roleName: string) => Promise<string | null>
   findUserByEmail: (email: string) => Promise<User | null>
+  login: (email: string, password: string) => Promise<User | null>
   findUserByPhone: (phone: string) => Promise<User | null>
   findUserByRegisterDocument: (registerDocument: string) => Promise<User | null>
 }
