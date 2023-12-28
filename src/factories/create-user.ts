1befc558e6bfd319157b8e6b6c8b66a7a685c6de
Caio Braga <caioaugustobrg@gmail.com>
2023-12-28 19:44:53 -0300

    :sparkles: feat: facotires

---
+++ src/factories/create-user.ts
@@ -0,0 +1,17 @@
+import { CreateUserController } from '../controllers/create-user-controller'
+import { CreateUserUseCase } from '../usecases/create-user/create-user'
+import { PrismaUserRepository } from '../external/repositories/prisma/prisma-user-repository'
+import EmailSender from '../external/mail-service/implementation/sendGridMailProvider'
+
+const prismaUserRepository = new PrismaUserRepository()
+// eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+const emailSender = new EmailSender(process.env.SENDGRID_API_KEY as string)
+const createUserUseCase = new CreateUserUseCase(
+  emailSender,
+  prismaUserRepository
+)
+const createUserController = new CreateUserController(
+  createUserUseCase
+)
+
+export { createUserController }
