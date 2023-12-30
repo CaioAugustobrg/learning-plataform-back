70932e6d4afe331efa7d7f38cfd94b4530de58c1
Caio Braga <caioaugustobrg@gmail.com>
2023-12-29 23:35:44 -0300

    :sparkles: feat: express session stores in redis

--- src/main/config/express-session.ts
+++ src/main/config/express-session.ts
@@ -1,11 +1,19 @@
 import { type Express } from 'express'
 import session from 'express-session'
-
+import RedisStore from 'connect-redis'
+import { createClient } from 'redis'
 export const setupExpressSession = (app: Express): void => {
+  const redisClient = createClient({
+    url: 'redis://username:password@redis-host:6379/0'
+  })
+  const redisStore = new RedisStore({
+    client: redisClient
+  })
   app.use(
     session({
       secret: 'learning-back',
       resave: false,
+      store: redisStore,
       saveUninitialized: false
     })
   )
