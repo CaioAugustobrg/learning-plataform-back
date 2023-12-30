c373e021b73a86bafe442838e51790631025f0ce
Caio Braga <caioaugustobrg@gmail.com>
2023-12-29 23:35:29 -0300

    :sparkles: feat: extended sessions types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -2,6 +2,6 @@ import 'express-session'
 
 declare module 'express-session' {
   export interface SessionData {
-    user: Record<string>
+    user: Record<string> // declara mas não inicializa
   }
 }
