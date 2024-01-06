70f8efb978a2c92ee06926611a10706745cdcc51
Caio Braga <caioaugustobrg@gmail.com>
2024-01-06 19:10:46 -0300

    :sparkles: feat: findUserbyId type

--- src/usecases/ports/user-repository.ts
+++ src/usecases/ports/user-repository.ts
@@ -2,8 +2,9 @@ import { type User } from '../../entities/user'
 
 export interface UserRepository {
   findUserByCpf: (cpf: string) => Promise<User | null>
-  create: (user: User, roleName: string) => Promise<string | null>
+  create: (user: User, roleName: string) => Promise<User | null>
   findUserByEmail: (email: string) => Promise<User | null>
+  findUserById: (userProfesorId: string) => Promise<User | null>
   login: (email: string, password: string) => Promise<User | null>
   findUserByPhone: (phone: string) => Promise<User | null>
   findUserByRegisterDocument: (registerDocument: string) => Promise<User | null>
