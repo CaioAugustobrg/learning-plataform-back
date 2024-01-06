e13be70944333925382004df995cd77d5ad849c2
Caio Braga <caioaugustobrg@gmail.com>
2024-01-06 19:12:33 -0300

    :recycle: refactor: changed return values

--- src/usecases/ports/course-repository.ts
+++ src/usecases/ports/course-repository.ts
@@ -2,6 +2,6 @@ import { type Course } from '../../entities/course'
 
 export interface CourseRepository {
   findCourseByTitle: (title: string) => Promise<Course | null>
-  create: (props: Course, userProfessorId: string) => Promise<void>
+  create: (props: Course, userProfessorId: string) => Promise<Course | null>
 
 }
