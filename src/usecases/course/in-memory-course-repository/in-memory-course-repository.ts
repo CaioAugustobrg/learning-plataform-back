b3b86caf859c79082269faf134f0c6bc34d78526
Caio Braga <caioaugustobrg@gmail.com>
2024-01-17 20:54:55 -0300

    :sparkles: feat: added delete course

--- src/usecases/course/in-memory-course-repository/in-memory-course-repository.ts
+++ src/usecases/course/in-memory-course-repository/in-memory-course-repository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { type UserProfessorCourse } from '@prisma/client'
 import { type Course } from '../../../entities/course'
@@ -5,12 +6,14 @@ import { type CourseRepository } from '../../ports/course-repository'
 
 export class InMemoryCourseRepository implements CourseRepository {
   courses: Course[] = []
-  userProfesorCourse: UserProfessorCourse[] = [{
-    courseId: '2',
-    createdAt: new Date(),
-    id: '1',
-    userId: '3'
-  }]
+  userProfesorCourse: UserProfessorCourse[] = [
+    {
+      courseId: '2',
+      createdAt: new Date(),
+      id: '1',
+      userId: '3'
+    }
+  ]
 
   constructor (courses: Course[]) {
     this.courses = courses
@@ -20,7 +23,7 @@ export class InMemoryCourseRepository implements CourseRepository {
     let course: Course
     for (course of this?.courses) {
       if (course.title === title) {
-        return course
+        return course as unknown as Course
       }
     }
     return null
@@ -37,19 +40,45 @@ export class InMemoryCourseRepository implements CourseRepository {
         createdAt: course.createdAt
       }
       this.userProfesorCourse.push(userProfessorCourseTable)
-      return course
+      return course as unknown as Course
     } else {
       return null
     }
   }
-//   async create (user: User): Promise<User | null> {
-//     const existsByCpf = await this.findUserByCpf(user.cpf)
-//     const existsByEmail = await this.findUserByEmail(user.email)
-//     if (!existsByCpf && !existsByEmail) {
-//       this.users.push(user)
-//       return user
-//     } else {
-//       return null
-//     }
-//   }
+
+  async deleteCourse (courseId: string): Promise<void> {
+    await this.deleteCourse(courseId)
+    this.courses.filter((course) => courseId !== course.id)
+  }
+
+  async findAll (): Promise<Course[] | null> {
+    const findAllCourses = await this.findAll()
+    if (!findAllCourses) {
+      return null
+    } else {
+      return this.courses as unknown as Course[]
+    }
+  }
+
+  async findCourseById (courseId: string): Promise<Course | null> {
+    const findCourseById = this.findCourseById(courseId)
+    if (!findCourseById) {
+      return null
+    } else {
+      return this.courses.filter(
+        (course) => courseId === course.id
+      ) as unknown as Course
+    }
+  }
+
+  //   async create (user: User): Promise<User | null> {
+  //     const existsByCpf = await this.findUserByCpf(user.cpf)
+  //     const existsByEmail = await this.findUserByEmail(user.email)
+  //     if (!existsByCpf && !existsByEmail) {
+  //       this.users.push(user)
+  //       return user
+  //     } else {
+  //       return null
+  //     }
+  //   }
 }
