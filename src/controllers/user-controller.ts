8ee0139aa365570e6d3b11d1303b87603529c170
Caio Braga <caioaugustobrg@gmail.com>
2024-02-08 19:50:47 -0300

    :sparkles: feat: github oauth

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
@@ -10,6 +11,10 @@ import jwt from 'jsonwebtoken'
 import { type TryRecoveryPasswordUseCase } from '../usecases/user/try-recover-password/try-recover-password'
 import { type PasswordRecoveryUseCase } from '../usecases/user/password-recovery/password-recovery'
 import { type DeleteUserUseCase } from '../usecases/user/delete-user/delete-user'
+import ApiError from '../utils/apiError'
+import axios from 'axios'
+import { type UserProps } from '../entities/user'
+// import fetch from 'node-fetch'
 
 export class UserController {
   constructor (
@@ -33,16 +38,12 @@ export class UserController {
       email,
       birthDate,
       phone,
-      cpf,
-      password,
-      education,
-      userRegisterDocument,
-      speciality
+      password
     } = httpRequest.body
     const roleName = httpRequest.params.roleName
     if (roleName === 'student') {
       try {
-        if (!name || !email || !birthDate || !phone || !password || !cpf) {
+        if (!name || !email || !birthDate || !phone || !password) {
           return response.status(400).json({
             msg: 'Todos os campos são obrigatórios'
           })
@@ -56,7 +57,7 @@ export class UserController {
       }
     } else {
       try {
-        if (!name || !email || !birthDate || !phone || !password || !cpf || !userRegisterDocument || !speciality || !education) {
+        if (!name || !email || !birthDate || !phone || !password) {
           return response.status(400).json({
             msg: 'Todos os campos são obrigatórios'
           })
@@ -147,5 +148,71 @@ export class UserController {
     } catch (error: any) {
       return response.status(error.code || 500).json(error.message)
     }
+  }
+
+  async userGithubLogin (request: Request, response: Response) {
+    const httpRequest = {
+      body: request.body,
+      params: request.params
+    }
+    const {
+      client_id,
+      client_secret,
+      code
+    } = httpRequest.body
+    const roleName = httpRequest.params.roleName
+    console.log(roleName)
+    console.log(httpRequest.body)
+
+    await axios.post(
+      'https://github.com/login/oauth/access_token',
+      {
+        client_id,
+        client_secret,
+        code
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        }
+      }
+    )
+      .then((result) => {
+        void axios.get('https://api.github.com/user', {
+          headers: {
+            Authorization: `Bearer ${result.data.access_token}`
+          }
+        }).then((result) => {
+          console.log(result.data)
+          const data: UserProps = {
+            name: result.data.name,
+            email: result.data.email,
+            githubId: result.data.id,
+            userRole: roleName,
+            phone: undefined,
+            birthDate: undefined,
+            password: undefined,
+            systemRole: 'CREATED'
+          }
+          const httpResponse = this.createUserStudentUseCase.handle(
+            data, roleName
+          )
+          return response.status(201).json(httpResponse)
+        }
+        )
+      }).catch((err: any) => {
+        if (err.response) {
+          console.error('GitHub API responded with an error:', err.response.data)
+        } else if (err.request) {
+          console.error('No response received from GitHub API:', err.request)
+        } else {
+          console.error('Error setting up the GitHub API request:', err.message)
+        }
+        throw new ApiError({
+          code,
+          error: err
+        })
+      })
   }
 }
