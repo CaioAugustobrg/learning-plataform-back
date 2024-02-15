98b4c6ed23e755c4f48a288ad854746a64b670bc
Caio Braga <caioaugustobrg@gmail.com>
2024-02-15 20:36:31 -0300

    :sparkles: feat: message repository

---
+++ src/external/repositories/rabbit-mq/rabbit-mq-message-repository.ts
@@ -0,0 +1,8 @@
+// import { Message } from "../../../domain/entities/rabbit-mq-message";
+// import { MessageRepository } from "../../../domain/usecases/ports/message-repository";
+
+// export class RabbitMqRepository implements MessageRepository {
+//     async sendMessage (props: Message): Promise<boolean> {
+
+//     }
+// }
