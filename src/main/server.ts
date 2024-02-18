7f0b7da8b1ca5e71e04a71ccea50d652d17d1623
Caio Braga <caioaugustobrg@gmail.com>
2024-02-18 19:35:45 -0300

    :bug: fix: fixed sending email

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,65 +1,38 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable @typescript-eslint/await-thenable */
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-// import { setupExpressSession } from './main/config/express-session'
-import router from './routes'
-import express from 'express'
-import session from 'express-session'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import redisClient from './config/redis-client'
-import connectRedis from 'connect-redis'
-const RedisStore = connectRedis(session)
-const redisStoreInstance = new RedisStore({ client: redisClient })
-// const redisClient = new IORedis(process.env.REDIS_URL ?? 'redis://127.0.0.1:6379')
-// import { setupExpressSession } from './main/config/express-session'
-// const redis = new Redis({
-//   host: '127.0.0.1', // Endereço do host do contêiner Docker
-//   port: 6379 // Porta padrão do Redis
-// })
-const app = express()
-app.use(cors({
-  origin: 'http://127.0.0.1:5173',
-  credentials: true
-}))
-app.use(express.json())
-app.use(router)
-app.use(cookieParser('keyboard cat'))
-
-app.use(
-  session({
-    name: 'teste',
-    store: redisStoreInstance,
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false,
-      // domain: process.env.COOKIE_DOMAIN,
-      sameSite: 'none',
-      maxAge: 20000
-    }
-  })
-)
-// app.use(
-//   expressSession({
-//     resave: false,
-//     saveUninitialized: true,
-//     secret: 'keyboard cat',
-//     cookie: { secure: false }
-//   })
-// )
+// main.ts
+import { type IMessage } from '../domain/usecases/ports/mail-service'
+import { generateWelcomeEmailToStudentContent } from '../external/mail-service/implementation/generate-email'
+import EmailSender from '../external/mail-service/implementation/sendGridMailProvider'
+import app from './config/app'
+import RabbitmqClient from './config/rabbit-mq-client'
+// import RabbitmqClient from './config/rabbit-mq-client'
 
 const port = 3030
 
-// Conectar ao servidor Redis
-
-// Define uma chave e um valor no Redis
-
-// Obtém o valor de uma chave do Redis
-
 app.listen(port, async () => {
-  redisClient.mset('222', 'asd22222222asdas')
-  console.log(`Express started on http://127.0.0.1:${port}; press CTRL + C to terminate.`)
+  try {
+    const server = new RabbitmqClient(process.env.RABBITMQ_URL!)
+    await server.start()
+    await server.consume('email-queue', async (message: any) => {
+      const response = message.content.toString()
+      const responseToJson = JSON.parse(response)
+      console.log(responseToJson)
+      const emailContent: IMessage = generateWelcomeEmailToStudentContent(responseToJson.email)
+      // const emailData = {
+      //   to: responseToJson.email,
+      //   from: responseToJson.from,
+      //   subject: responseToJson.subject
+      //   // html: new DOMParser().parseFromString(responseToJson.html, 'text/html')
+      // }
+      console.log('server', emailContent)
+      const emailSender = new EmailSender(process.env.SENDGRID_API_KEY!)
+      await emailSender.sendEmail(emailContent)
+    }
+    )
+    console.log(`Express started on http://127.0.0.1:${port}; press CTRL + C to terminate.`)
+  } catch (error) {
+    console.error('Error connecting to Redis:', error)
+  }
 })
