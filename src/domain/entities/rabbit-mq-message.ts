aab053e11dc335fa306755b569b656d899ae3b58
Caio Braga <caioaugustobrg@gmail.com>
2024-02-18 19:37:06 -0300

    :recycle: refactor: reviwed attributes

--- src/domain/entities/rabbit-mq-message.ts
+++ src/domain/entities/rabbit-mq-message.ts
@@ -3,17 +3,14 @@ import { Entity } from '../core/Entity'
 export interface MessageProps {
   email: string
   name: string
-  text: string
 }
 
 export class Message extends Entity<MessageProps> {
   public email: string
   public name: string
-  public text: string
   public constructor (props: MessageProps) {
     super()
     this.email = props.email
     this.name = props.name
-    this.text = props.text
   }
 }
